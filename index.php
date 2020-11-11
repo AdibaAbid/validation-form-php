a02ee2a5be6df15784ffa12b3ed32c3b9fbc9a1c
AdibaAbid <adiba.abid_@hotmail.com>
2020-11-11 09:30:49 +0500

    previous work

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include('templates/header.php')?>
+    
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <title>Simple UI</title>
+</head>
+
+<body class='light-blue lighten-5'>
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo deep-purple-text">Testing</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn light-blue darken-4 z-depth-0">Button</a></li>
+            </ul>
+        </div>
+    </nav>
